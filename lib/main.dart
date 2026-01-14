40d6c5708e92f9adb27ae710e9fa4617bdbede7c
Udayan Basak <thisisudayan@gmail.com>
2026-01-14 14:29:26 +0600

    ...

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:olive_app/app/view/app.dart';
+import 'package:olive_app/app/app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
--- lib/view_models/customer_view_model.dart
+++ lib/view_models/customer_view_model.dart
@@ -75,3 +75,4 @@ class CustomerViewModel extends ChangeNotifier {
     }
   }
 }
+ 
